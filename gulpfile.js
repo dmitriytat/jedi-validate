126e81a1d5fa786304da032f56bd35a8cc293790
Dmitriy Tatarintsev <tatarintsevd@gmail.com>
2016-08-19 14:06:20 +0300

    Rename js file to jedi-validate

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,22 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     sourcemaps = require('gulp-sourcemaps'),
     uglify = require('gulp-uglify'),
-    rename = require("gulp-rename");
+    rename = require("gulp-rename"),
+    umd = require("gulp-umd"),
+    babel = require('gulp-babel');
 
 gulp.task('default', function () {
     return gulp.src([
-            './src/jdvalidate.js',
+            './src/jedi-validate.es6',
             './src/methods.js',
             './src/**/*'])
         .pipe(plumber())
         .pipe(sourcemaps.init())
-        .pipe(concat('jdvalidate.js'))
+        .pipe(concat('jedi-validate.js'))
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        // .pipe(umd())
         .pipe(gulp.dest('./dist'))
         .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
